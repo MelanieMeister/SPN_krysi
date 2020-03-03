89fad85ddd068baa72bf51aa44694116ec30ce03
Meister <melanie.meister@students.fhnw.ch>
2020-03-03 09:01:51 +0100

    initial

---
+++ src/main/java/view/ViewMixin.java
@@ -0,0 +1,32 @@
+package view;
+
+/**
+ * @author Dieter Holz
+ */
+public interface ViewMixin {
+
+	default void init() {
+        initializeSelf();
+        initializeControls();
+		layoutControls();
+		setupEventHandlers();
+		setupValueChangedListeners();
+		setupBindings();
+	}
+
+	default void initializeSelf(){
+    }
+
+	void initializeControls();
+
+	void layoutControls();
+
+	default void setupEventHandlers() {
+	}
+
+	default void setupValueChangedListeners() {
+	}
+
+	default void setupBindings() {
+	}
+}
